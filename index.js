3fba4901e45106861fa77537dd8635f0e9d40629
mashraf01 <106184896+mashraf01@users.noreply.github.com>
2023-04-28 19:39:32 -0400

    semi working

--- index.js
+++
@@ -1,4 +0,0 @@
-// index.js
-import { AnnotationCanvas } from "./src/annotationCanvas";
-
-export { AnnotationCanvas };
---
+++ src/index.js
@@ -0,0 +1,4 @@
+// index.js
+import { AnnotationCanvas } from "./src/annotationCanvas";
+
+export { AnnotationCanvas };
